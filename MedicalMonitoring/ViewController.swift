7bd622c35753ecae3d1cab3cd854c3c8bd644370
Franck <ufrank238@yahoo.fr>
2020-07-20 13:50:31 +0200

    Initial Commit

---
+++ MedicalMonitoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicalMonitoring
+//
+//  Created by ema on 20.07.20.
+//  Copyright © 2020 ema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
